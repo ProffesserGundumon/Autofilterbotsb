95dcbac3c3edce0f93fc7dce8f41bb00649fef11
ProffesserGundumon <91726657+ProffesserGundumon@users.noreply.github.com>
2021-10-02 17:15:22 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -57,9 +57,10 @@ async def start(bot, update):
     
     reply_markup = InlineKeyboardMarkup(buttons)
     
-    await bot.send_message(
+    await bot.send_photo(
         chat_id=update.chat.id,
-        text=Translation.START_TEXT.format(
+        photo=https://telegra.ph/file/f5a1207d0cb4e85f59892.jpg
+        caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
         reply_markup=reply_markup,
         parse_mode="html",
