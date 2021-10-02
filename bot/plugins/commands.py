6f6b9d286356ee56f80d3f0ac256c1611dd25d96
ProffesserGundumon <91726657+ProffesserGundumon@users.noreply.github.com>
2021-10-02 17:16:59 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -59,7 +59,7 @@ async def start(bot, update):
     
     await bot.send_photo(
         chat_id=update.chat.id,
-        photo=https://telegra.ph/file/f5a1207d0cb4e85f59892.jpg
+        photo="https://telegra.ph/file/f5a1207d0cb4e85f59892.jpg",
         caption=Translation.START_TEXT.format(
                 update.from_user.first_name),
         reply_markup=reply_markup,
